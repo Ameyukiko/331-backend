59401aafb4390c98aad643a6155ceff14be1d611
Ameyukiko <Aphichaya_sup@cmu.ac.th>
2025-09-04 13:44:10 +0700

    4.4

--- src/main/java/se331/lab/Application.java
+++ src/main/java/se331/lab/Application.java
@@ -19,7 +19,7 @@ public class Application {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/events")
+				registry.addMapping("/events/*")
 						.allowedOrigins("http://localhost:5173");
 			}
 		};
