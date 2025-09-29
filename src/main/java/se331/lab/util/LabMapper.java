88a74c1233837d939734ec78ea26b64952432520
Ameyukiko <Aphichaya_sup@cmu.ac.th>
2025-09-29 16:08:58 +0700

    fix complete 3.6

--- src/main/java/se331/lab/util/LabMapper.java
+++ src/main/java/se331/lab/util/LabMapper.java
@@ -3,7 +3,9 @@ package se331.lab.util;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 import se331.lab.DTO.EventDTO;
+import se331.lab.DTO.EventOrgainzerDTO;
 import se331.lab.entity.Event;
+import se331.lab.entity.Organizer;
 
 import java.util.List;
 
@@ -12,4 +14,7 @@ public interface LabMapper {
     LabMapper INSTANCE = Mappers.getMapper(LabMapper.class);
     EventDTO getEventDto(Event event);
     List<EventDTO> getEventDto(List<Event> events);
+
+    // เพิ่ม mapping สำหรับ organizer
+    EventOrgainzerDTO getEventOrgainzerDto(Organizer organizer);
 }
