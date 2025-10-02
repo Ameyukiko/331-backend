eb3e22e9f5bfa54678fa8d63bc82f45ce31986ab
Ameyukiko <Aphichaya_sup@cmu.ac.th>
2025-10-02 22:18:17 +0700

    fix to complete 7

--- src/main/java/se331/lab/DTO/EventOrganizerDTO.java
+++ src/main/java/se331/lab/DTO/EventOrganizerDTO.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class EventOrgainzerDTO {
+public class EventOrganizerDTO {
     Long id;
     String name;
 }
